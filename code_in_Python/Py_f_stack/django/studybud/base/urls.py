ad5c98d3b749177e07d4fa389cdc04aca2bc2a31
Shashank Raj <shashank1_theone@yahoo.co.in>
2025-07-25 20:15:51 +0530

    Removing redunadnt code

--- code_in_Python/Py_f_stack/django/studybud/base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path  ## So that we cpecify paths
-from . import views
-
-urlpatterns = [
-    path('',views.home_page, name="home"),
-    path('rooms/',views.rooms,name="rooms"),
-]
